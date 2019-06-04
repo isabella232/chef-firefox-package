effd82d117bc0f9facba808d8dbdaa75386d9f75
Eric Romani <eric_romani@rapid7.com>
2019-06-04 14:40:47 -0700

    Updating after use in nexpose jenkins cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'r7_re@rapid7.com'
 license          'Apache 2.0'
 description      'Installs/Configures firefox_package'
 long_description 'Installs/Configures firefox_package'
-version          '0.5.1'
+version          '0.6.1'
 
 supports 'windows'
 supports 'ubuntu'
 
-depends 'poise', '~> 2.5.0'
+depends 'poise', '~> 2.8.0'
 depends 'windows'
 depends 'build-essential'
